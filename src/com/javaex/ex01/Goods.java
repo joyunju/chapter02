cc13fa6342d0527df1cd41213088189b86af0023
joyunju <whdbswn1113@naver.com>
2022-04-24 23:29:46 +0900

    (default)

--- src/com/javaex/ex01/Goods.java
+++ src/com/javaex/ex01/Goods.java
@@ -2,6 +2,17 @@ package com.javaex.ex01;
 
 public class Goods {
 	
+	//필드
 	String name;
 	int price;
+	
+	//생성자
+	
+	//메소드 - 겟터세터
+	
+	//메소드 - 일반
+//	public static void main(String[] args) {
+//		메모리 운영 스토리 작성 
+//	}
+	
 }
---
+++ src/com/javaex/ex02/Goods.java
@@ -0,0 +1,19 @@
+package com.javaex.ex02;
+
+public class Goods {
+	
+	//필드
+	//private --> 캡슐화 정보 은닉을 위한 방법 
+	//주로 객체에 대한 정보(필드와 메소드)의 접근을 제한하기 위해 사용
+	//private String name;
+	
+	String name;
+	int price;
+	
+	//생성자
+	
+	//메소드 - 겟터세터
+	
+	//메소드 - 일반
+	
+}
