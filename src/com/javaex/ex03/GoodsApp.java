db310d6eaea5c2a8a4af3e32b70d6b6ec2ae82cd
joyunju <whdbswn1113@naver.com>
2022-04-25 02:58:16 +0900

    메소드 호출

--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -5,11 +5,22 @@ package com.javaex.ex03;
 public class GoodsApp {
 
 	public static void main(String[] args) {
-
+		
+		//camera
 		Goods camera = new Goods();
-
 		camera.setName("니콘");
 		camera.setPrice(400000);
+		
+		//computer
+		Goods computer = new Goods();
+		computer.setName("LG그램");
+		computer.setPrice(900000);
+		
+		//cup
+		Goods cup = new Goods();
+		cup.setName("머그컵");
+		cup.setPrice(2000);
+		
 
 		// 출력
 		String cameraName = camera.getName();
@@ -20,7 +31,10 @@ public class GoodsApp {
 		int cameraPrice = camera.getPrice();
 		System.out.println(cameraPrice);
 		
+		//출력
 		System.out.println("상품이름 : " + camera.getName() + ", 가격 : " + camera.getPrice());
+		System.out.println("상품이름 : " + computer.getName() + ", 가격 : " + computer.getPrice());
+		System.out.println("상품이름 : " + cup.getName() + ", 가격 : " + cup.getPrice());
 
 	}
 
