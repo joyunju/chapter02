63423b7cb0be38ebc92b42abe569f6ef54a1d2e3
joyunju <whdbswn1113@naver.com>
2022-04-28 00:15:19 +0900

    loading

---
+++ src/com/javaex/ex10/GoodsApp.java
@@ -0,0 +1,19 @@
+package com.javaex.ex10;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+
+		Goods camera = new Goods("니콘", 400000);
+		Goods computer = new Goods("LG그램", 900000);
+		Goods cup = new Goods("머그컵", 2000);
+
+
+
+		// 출력
+		System.out.println("======출력======");
+
+
+	}
+
+}
